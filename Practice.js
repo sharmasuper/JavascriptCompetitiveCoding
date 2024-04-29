9493afb1d1534ac7f4587c0496a587d9cea4805d
sharmasuper <mohit6375sharma@gmail.com>
2024-04-29 06:42:37 +0530

    array at() method practice.html and js fle dekhn

---
+++ Practice.js
@@ -0,0 +1,7 @@
+//javascrit array at method 
+const array = ["MOhit","Ram","Shyam","Govind"]
+console.log(array)
+// console.log(array[array.length-1])
+console.log(array.at(-3))
+
+console.log(array.at(3))
