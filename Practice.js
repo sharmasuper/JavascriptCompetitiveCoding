0e8eefe79d8e23494ad0c8bb0c647de2c1efe094
sharmasuper <mohit6375sharma@gmail.com>
2024-04-29 09:22:40 +0530

    array fill method

--- Practice.js
+++ Practice.js
@@ -1,25 +1,9 @@
-//Array.every()
-// const arr = [2,1,2,3,4,5,6]
-const arr = [2,,4,6,8]
-const er = arr.every((e)=>{
-    if(e%2===0){
-        return e
-    }
-})
+//array fill method
+// array.fill(value, start, end)
+const array = ["Mohit","Ram","Shyam",1,2,3,4]
+array.fill("Laddu",2,3)
+console.log(array)
 
-console.log(er)
 
-//every method sabhi mai check karha h ki or return true or false kartha h
-const arr1 = [2,4,6,8]
-const ev = arr1.every((item)=>{
-      return item>1
-})
- console.log(ev)
 
-const person = [
-    {name:"Mohit"},{name:"Ram"},{name:"Gopal"},{name:"ms"}
-]
-
-const check = person.every((item)=>item.name!==undefined)
- console.log(check)
 
