db8ced0a81758d7103bb97e8e9092dd9faef7084
sharmasuper <mohit6375sharma@gmail.com>
2024-04-29 08:32:16 +0530

    array entries() method

--- Practice.js
+++ Practice.js
@@ -1,32 +1,22 @@
-//javascrit array at method 
+//array.entries()
+const array = ['Mohit',"sharma","Ram"] 
+R = array.entries()  //yai array ko itrator mai convert kartha h
 
-// const array2 = [1,2,3,4]
-// const array1 = ["MOhit","Ram","Shyam","Govind"]
+console.log(R)
+
+for (e of R) {
+    console.log(e)
+}
+
+//output - // (2) [0, 'Mohit']
+// Practice.js:6 (2) [1, 'sharma']
+// Practice.js:6 (2) [2, 'Ram']
  
+for (e of array){
+    console.log(e)  
+}
 
-
-// const combine = array1.concat(array2)
-// console.log(combine)
-
-
-const nums = [0,1,2,3,4,5,6,7,8,9]
-nums.copyWithin(0,3,4)
-console.log(nums)
-nums.copyWithin(0)
-console.log(nums)
-nums.copyWithin(1)
-console.log(nums) output- [0, 0, 1, 2, 3, 4, 5, 6, 7, 8]
-
-nums.copyWithin(2,3,5)
-console.log(nums) output [0, 1, 3, 4, 4, 5, 6, 7, 8, 9]
-
-//copywithin(startIndex,startcopyIndex,endcopyIndex)
-//startIndex = startIndex jaha sai aap copy index ko start karna chahtai ho
-
-//startCopyindex = jaha sai aap index ko copy karna chahtai ho
-
-//endcopyIndex = jaha sai aap copy karnai valai index ko close karna chahtai ho
-
-
-console.log("hello")
-
+// //output
+// Mohit
+// Practice.js:13 sharma
+// Practice.js:13 Ram
