c98b46a19fac7237984a9a10d26449c0ba8b5c95
sharmasuper <mohit6375sharma@gmail.com>
2024-04-29 09:38:25 +0530

    filter method in array

--- Practice.js
+++ Practice.js
@@ -1,9 +1,30 @@
-//array fill method
-// array.fill(value, start, end)
-const array = ["Mohit","Ram","Shyam",1,2,3,4]
-array.fill("Laddu",2,3)
-console.log(array)
+// array.filter(callbackFunction(element, index, array), thisArg)
+// callbackFunction: यह एक फ़ंक्शन है जो हर एरे आइटम के लिए कॉल होती है।
+// element: वर्तमान आइटम का मान।
+// index (वैकल्पिक): वर्तमान आइटम का इंडेक्स।
+// array (वैकल्पिक): वह एरे जिसमें फ़िल्टर किया जा रहा है।
+// thisArg (वैकल्पिक): callbackFunction को कॉल करने के लिए this के मान को सेट करने के लिए उपयोग किया जाता है।
+//return new Array after filter
 
+const array = [1, 2, 3, 4, 5];
+const add = array.filter((item)=>{
+      return item>2
+})
+console.log(add)
+
+
+const array2 = [1,"Two",3,"four",5]
+      
+  const add2 =     array2.filter((item)=>typeof item === "string")
+      console.log(add2)
+
+     const add3 = array2.filter((item)=>{
+        if(typeof item ==="number"){
+          return item
+        }
+     })
+//return new Array after filter
+     console.log(add3)
 
 
 
