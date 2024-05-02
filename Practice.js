f1fe8e8106064d687a80a4bcff95ad85e35044df
sharmasuper <mohit6375sharma@gmail.com>
2024-05-02 05:57:46 +0530

    cookies topic

--- Practice.js
+++ Practice.js
@@ -1,17 +1,24 @@
 
-let key = prompt("Enter key you want to set")
-let value = prompt('Enter value you want to set')
+//cookies
+console.log(document.cookie)
+document.cookie = "name=harry1122334400"
+document.cookie = "name2=harry11223445502"  //cookie replace nahi hotai yai add ho jatai h
+document.cookie = "name=MOhit1122334400"    //esai hum issai update kar sktai h
+console.log(document.cookie)
 
-localStorage.setItem(key,value)
+///ab tiwitter.com per jao or vaha per cookies ko update krao
+//or console per likhna h
+//document.cookie   
+//command - console.log(document.cookie)  issai uskai sarai cookie mil jaygai
+// new topic  
+//incode URI component
+let key  = prompt("enter your key")
+let value  = prompt("enter your value")
 
-console.log(`The value at ${key} is ${localStorage.getItem(key)}`)
- 
-if(key == "pass" || key == "fail"){
-   localStorage.removeItem(key)
-}
+document.cookie = `${key} = ${value}`  //yai ;#4: esai key value pair ko set nahi kar paya
+console.log(document.cookie)           //isko solve karnai kai liy encodeURIComponent ka use kartai h it takes all string
 
-if(key == 0){
-   localStorage.clear()
-}
+document.cookie = `${encodeURIComponent(key)} = ${encodeURIComponent(value)}`
+console.log(document.cookie)
 
 
