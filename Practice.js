537dda7cff4c6f64af627d95d3fe17b3c1838bef
sharmasuper <mohit6375sharma@gmail.com>
2024-05-03 21:03:09 +0530

    Localstorage

--- Practice.js
+++ Practice.js
@@ -1,10 +1,10 @@
 //session storage
 
 
-// sessionStorage.getItem("name")
-// sessionStorage.clear()
-// sessionStorage.setItem('name','harry')
-// sessionStorage.removeItem("name")
+sessionStorage.getItem("name")
+sessionStorage.clear()
+sessionStorage.setItem('name','harry')
+sessionStorage.removeItem("name")
 
 localStorage.setItem("name","Mohit")
 localStorage.setItem("names","Harry2")
