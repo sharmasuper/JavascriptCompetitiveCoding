b9447bf564ce0a9ddd15b367c019718d31bbf841
sharmasuper <mohit6375sharma@gmail.com>
2024-06-04 22:56:37 +0530

    argunment method in function

--- Static.js
+++ Static.js
@@ -1,10 +1,19 @@
-// [1, 2, 3, 4, 5].map(function (n) {
-//    return n <= 1 ? 1 : arguments.callee(n - 1) * n;
-//  });
+// a = [1,2,3,5].map(function(n){
+//    return  n<=1?1 :arguments.callee(n-1)*n
+// })
+// console.log(a)
+
+function foo(n) {
+   const f = () => arguments[0]+n ; // foo's implicit arguments binding. arguments[0] is n
+   return f();
+ }
+ 
+ foo(3); // 3 + 3 = 6
+//  argunment - 
+//  Arguments(2) [3, 2, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+ 
+ add = foo(3); // 3 + 3 = 6
+ console.log(add)
+ 
 
 
-e = [1,2,3,4,5]
-a = e.map(function(n){
-   return n<=1?1:arguments.callee(n-1)*n
-})
-console.log(a)
