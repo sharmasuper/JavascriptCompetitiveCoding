fd6e957e23a41b597a8b2c298ace7b6c65eee01c
sharmasuper <mohit6375sharma@gmail.com>
2024-06-05 01:48:09 +0530

    function.displayname property

--- Static.js
+++ Static.js
@@ -1,56 +1,13 @@
-// Create a global property with `var`
-var x = 10;
+// const a = function () {};
+// a.displayName = "MyFunction";
 
-function createFunction1() {
-  const x = 20;
-  return new Function("return x;"); // this `x` refers to global `x`
-}
+// a; // function MyFunction()
+// console.log(a.displayName())
+ add = function some(a,b){
+   return a+b
+ }
+// some.displayName = "sharma"
+// console.log(some.displayName)
 
-function createFunction2() {
-  const x = 20;
-  function f() {
-     // this `x` refers to the local `x` above
-     function f11(){
-      return x
-     }
-     return f11()
-   }
-  function f3(){
-   return x+2
-  }
-  
-  return f()+f3();
-}
-const f2 = createFunction2()
-console.log(f2)
-      
-
-// const f1 = createFunction1();
-// console.log(f1()); // 10
-// const f2 = createFunction2();
-// console.log(f2()); // 20
-
-// const f1 = createFunction1()
-// console.log(f1())
-
-
-// Create a global property with `var`
-var x = 10;
-
-function createFunction1() {
-  const x = 20;
-  return new Function("return x;"); // this `x` refers to global `x`
-}
-
-function createFunction2() {
-  const x = 20;
-  function f() {
-    return x; // this `x` refers to the local `x` above
-  }
-  return f;
-}
-
-const f1 = createFunction1();
-console.log(f1()); // 10
-const f2 = createFunction2();
-console.log(f2()); // 20
+add.displayName = "hello"
+console.log(add.displayName)
