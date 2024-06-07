2742b289c4a358d3bc18f51c96324354da4d8478
sharmasuper <mohit6375sharma@gmail.com>
2024-06-07 13:28:15 +0530

    generator

--- Static.js
+++ Static.js
@@ -1,11 +1,54 @@
-const language = {
-   set current(name) {
-     this.log.push(name);
-   },
-   log: [],
- };
- 
- language.current = 'EN';
- language.current = 'FA';
- 
- console.log(language.log);
+// try{
+
+// const foo = function *(){
+//   yield 'a';
+//   yield 'b';
+//   yield 'c';
+// };
+// let str = ''
+// for(const val of foo()){
+//  str = str+val
+// }
+// console.log(str)
+// }catch(error){
+//   console.log("add Error",error)
+// }
+
+
+
+try{
+  
+   const foo = function *(){
+    yield 'a';
+    yield 'b';
+    yield 'c';
+   }
+   a = foo()
+   console.log(a.entries())
+  //  console.log(a.next())
+  //  console.log(a.next())
+  //  console.log(a.next())
+
+
+  function* infinite() {
+    let index = 0;
+  
+    while (true) {
+      yield index++;
+    }
+  }
+  add = infinite()
+  // console.log(add.next())
+  // console.log(add.next())
+  // console.log(add.next())
+  
+
+
+}catch(error){
+  console.log("show error",error) 
+}
+
+
+
+
+
