193395faa77237245ed897053940342efbd54cbd
sharmasuper <mohit6375sharma@gmail.com>
2024-06-07 15:20:12 +0530

    escape() method

--- Static.js
+++ Static.js
@@ -1,4 +1,10 @@
-const uri = 'https://mozilla.org/?x=шеллы';
-const encoded = encodeURIComponent(uri); 
-console.log(encoded);
-console.log("encodeuri",encodeURI(uri))
+// The escape() method in JavaScript is an older function that encodes a string so that it can be safely 
+// included in a URL. It encodes special characters, except *, +, -, . and @, using the percent-encoding scheme. 
+// However, it has been deprecated and replaced by encodeURI() and encodeURIComponent(), 
+// which are more robust and versatile for handling URL components.
+
+
+e = escape("hello mohit sharma")
+console.log(e)
+a = decodeURI(e)
+console.log(a)
