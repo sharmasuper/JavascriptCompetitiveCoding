c6348e5d6a3a94cc9e5c77405eaab661c1169a58
sharmasuper <mohit6375sharma@gmail.com>
2024-06-07 14:52:26 +0530

    decodeurl vs decodeuricomponent

--- Static.js
+++ Static.js
@@ -1,4 +1,14 @@
-const uri = 'mohit sharma'; //'https://mozilla.org/?x=шеллы'
-const encoded = encodeURI(uri)
-console.log(encoded)
-console.log(decodeURI(encoded))
+function containEncodedComponent(x){
+  return decodeURI(x) !== decodeURIComponent(x)
+}
+
+// console.log(
+// containEncodedComponent("https://www.google.com/search?q=%E0%A4%B8%E0%A5%8D%E0%A4%B0%E0%A5%87%E0%A4%B5%E0%A4%BF%E0%A4%95%E0%A4%BE%E0%A4%B0"))
+
+e = decodeURIComponent("%3Fx%3Dtest")
+f = decodeURI('%3Fx%3Dtest')
+console.log(f)
+console.log(e)
+
+
+
