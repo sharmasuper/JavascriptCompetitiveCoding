0ded423ea1b5a5bcbca8f329ac4ceb2fdb2a8cb1
sharmasuper <mohit6375sharma@gmail.com>
2024-06-12 23:42:23 +0530

    Intl.DisplayNames.prototype.resolvedOptions()

--- Static.js
+++ Static.js
@@ -1,40 +1,12 @@
-// Certainly! The Intl.DisplayNames.prototype.of() method is used to return a localized string for a 
-// given code, such as a region code, language code, script code, or currency code. 
-// Below are some examples demonstrating how to use the of() method for different types of codes.
+// Certainly! The Intl.DisplayNames.prototype.resolvedOptions() method is used to return an object with properties reflecting
+//  the locale and options computed during the initialization of the Intl.DisplayNames object.
 
-// const regionDisplayNames = new Intl.DisplayNames(['en'],{type:'region'})
+// Create a new Intl.DisplayNames object for the 'en' locale with type 'region'
+const displayNames = new Intl.DisplayNames(['en'], { type: 'region', fallback: 'code' });
 
-// console.log(regionDisplayNames.of('US'))
-// console.log(regionDisplayNames.of('FR'))
-// console.log(regionDisplayNames.of('JP'))
+// Use the resolvedOptions() method to get the computed options
+const options = displayNames.resolvedOptions();
 
-
-// Create a new Intl.DisplayNames object for the 'en' locale with type 'language'
-const languageDisplayNames = new Intl.DisplayNames(['en'], { type: 'language' });
-
-// Use the of() method to get the display name for specific language codes
-console.log(languageDisplayNames.of('en'));  // Output: "English"
-console.log(languageDisplayNames.of('fr'));  // Output: "French"
-console.log(languageDisplayNames.of('ja'));  // Output: "Japanese"
-
-
-
-
-// Create a new Intl.DisplayNames object for the 'en' locale with type 'script'
-const scriptDisplayNames = new Intl.DisplayNames(['en'], { type: 'script' });
-
-// Use the of() method to get the display name for specific script codes
-console.log(scriptDisplayNames.of('Latn'));  // Output: "Latin"
-console.log(scriptDisplayNames.of('Cyrl'));  // Output: "Cyrillic"
-console.log(scriptDisplayNames.of('Arab'));  // Output: "Arabic"
-
-
-
-
-
-
-
-
-
+console.log(options);
 
 
