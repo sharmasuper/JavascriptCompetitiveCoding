eb676700c25ce6746fc85cb13054e4ff7014d584
sharmasuper <mohit6375sharma@gmail.com>
2024-06-12 22:07:02 +0530

    Intl.DateTimeFormat.supportedLocalesOf()

--- Static.js
+++ Static.js
@@ -1,25 +1,20 @@
-// Certainly! The Intl.DateTimeFormat.prototype.resolvedOptions() method in JavaScript 
-// is used to return a new object with properties reflecting the locale and date and time formatting options 
-// computed during the initialization of the Intl.DateTimeFormat object.
+// Sure! The Intl.DateTimeFormat.supportedLocalesOf() method is used to check which 
+// of the given locales are supported by the Intl.DateTimeFormat object.
 
-// Create a new Intl.DateTimeFormat object
-const dateTimeFormat = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    weekday: 'long',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    timeZone: 'America/New_York'
-  });
-  
-  // Use the resolvedOptions method to get the computed options
-  const options = dateTimeFormat.resolvedOptions();
-  
-  console.log(options);
-  
+// Define an array of locales
+// Define an array of locales
+const locales1 = ['ban', 'id-u-co-pinyin', 'de-ID'];
+const options1 = { localeMatcher: 'lookup' };
+
+console.log(Intl.DateTimeFormat.supportedLocalesOf(locales1, options1));
 
 
 
+// Define an array of locales
+const locales = ['en-US', 'fr-FR', 'de-DE', 'zh-CN', 'xyz-ABC'];
+
+// Check which of these locales are supported
+const supportedLocales = Intl.DateTimeFormat.supportedLocalesOf(locales);
+
+console.log(supportedLocales);
 
