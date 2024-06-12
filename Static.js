1d8cec835c11197dde5616c5d4e61de9af919018
sharmasuper <mohit6375sharma@gmail.com>
2024-06-12 21:09:53 +0530

    Intl.DateTimeFormat.prototype.formatRange()

--- Static.js
+++ Static.js
@@ -1,21 +1,22 @@
-// The Intl.Collator.supportedLocalesOf() method in JavaScript is used to check which of the 
-// specified locales are supported by the Intl.Collator object. This can be useful when you want to see if certain locales can 
-// be used for string comparison and sorting operations in a specific environment.
-
-// const locals = ['en-US', 'fr-FR', 'es-ES', 'de-DE', 'ja-JP'];
-// const supportedLocales = Intl.Collator.supportedLocalesOf(locals)
-// console.log(supportedLocales)
-// const list = ["mohit","mriyanka","mijaya"]
-// list.sort(new Intl.Collator('es-Fs').compare)
-// console.log(list)
+// The Intl.DateTimeFormat.prototype.formatRange() method in JavaScript is used to format a range of dates 
+// in a locale-sensitive manner. 
+// This method is useful for displaying date ranges in a way that is appropriate for the user's locale.
 
 
-const date = new Date()
-const formatter = new Intl.DateTimeFormat('en-us')
-// console.log(formatter)
-const formatterDate = formatter.format(date)
-console.log(formatterDate)
 
+const startDate = new Date('2024-06-01T00:00:00Z')
+const endDate = new Date('2025-08-17T23:59:59Z');
 
+const formatter = new Intl.DateTimeFormat('en-US',{
+    year:'numeric',
+    month:'long',
+    day:'numeric'
+})
+
+const formattedRange = formatter.formatRange(startDate,endDate)
+console.log(formattedRange);
+
+// const fomattedRange = formatter.format(startDate)
+// console.log(fomattedRange)
 
 
