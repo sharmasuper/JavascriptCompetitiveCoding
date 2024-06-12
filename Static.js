d0b47667be55ae31b443aff0602b9aeb5315cda9
sharmasuper <mohit6375sharma@gmail.com>
2024-06-12 21:18:27 +0530

    Intl.DateTimeFormat.prototype.formatRangeToParts()

--- Static.js
+++ Static.js
@@ -1,22 +1,10 @@
-// The Intl.DateTimeFormat.prototype.formatRange() method in JavaScript is used to format a range of dates 
-// in a locale-sensitive manner. 
-// This method is useful for displaying date ranges in a way that is appropriate for the user's locale.
-
-
-
-const startDate = new Date('2024-06-01T00:00:00Z')
-const endDate = new Date('2025-08-17T23:59:59Z');
-
-const formatter = new Intl.DateTimeFormat('en-US',{
+const startDate = new Date('2024-06-01T00:00:00Z');
+const endDate = new Date('2024-06-12T23:59:59Z');
+const formatter = new Intl.DateTimeFormat('ja-JP',{
     year:'numeric',
-    month:'long',
-    day:'numeric'
+    month:'2-digit',
+    day:'2-digit'
 })
 
 const formattedRange = formatter.formatRange(startDate,endDate)
-console.log(formattedRange);
-
-// const fomattedRange = formatter.format(startDate)
-// console.log(fomattedRange)
-
-
+console.log(formattedRange)
