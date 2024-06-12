d3d3d813ea55bd7208f050e02ce47668a3088d4e
sharmasuper <mohit6375sharma@gmail.com>
2024-06-12 21:59:18 +0530

    Intl.DateTimeFormat.prototype.resolvedOptions()

--- Static.js
+++ Static.js
@@ -1,30 +1,24 @@
-// The Intl.DateTimeFormat.prototype.formatToParts() method in JavaScript is used to 
-// format a date into an array of objects, each representing a part of the formatted date string. This method provides a detailed breakdown of the formatted date, 
-// allowing for granular control over the formatting and rendering of dates.
+// Certainly! The Intl.DateTimeFormat.prototype.resolvedOptions() method in JavaScript 
+// is used to return a new object with properties reflecting the locale and date and time formatting options 
+// computed during the initialization of the Intl.DateTimeFormat object.
 
-const date = new Date('2024-06-12T13:15:30Z');
-const formatter = new Intl.DateTimeFormat('en-US',{
-    year:'numeric',
-    month:'long',
-    day:'numeric',
-    weekday:'long',
-    hour:'numeric',
-    minute:'numeric',
-    second:'numeric',
-    timeZoneName:'short'
-})
-
-const formattedParts = formatter.formatToParts(date)
-console.log(formattedParts)
-
-// const formattedParts = formatter.format(date)
-// console.log(formattedParts)
-
-
-
-
-
-
+// Create a new Intl.DateTimeFormat object
+const dateTimeFormat = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'America/New_York'
+  });
+  
+  // Use the resolvedOptions method to get the computed options
+  const options = dateTimeFormat.resolvedOptions();
+  
+  console.log(options);
+  
 
 
 
